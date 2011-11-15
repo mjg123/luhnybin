2a28b1ab6d28fc85026cde969b55ab2d60f07225
Matthew Gilliard <matthew.2.gilliard@nokia.com>
2011-11-15 11:45:58 +0000

    playing with luhn

--- src/luhny/core.clj
+++ src/luhny/core.clj
@@ -1,17 +1,40 @@
 (ns luhny.core
   (:gen-class))
 
+(defn trim [s]
+  (apply str (butlast s)))
 
-(defn work 
+(defn luhny? [strin]
+  (zero?
+   (mod
+    (reduce
+     +
+     (map #(Integer. (str %))
+          (apply
+           str
+           (map-indexed
+            #(* (Integer. (str %2)) (if (even? %1) 2 1))
+            strin))))
+    10)))
+
+(defn block-luhn [strin]
+  (if (luhny? strin)
+    (repeat "X" (count strin))
+    strin))
+
+(defn work
   "Do all your work in here."
   [input]
+;;  (block-luhn input))
   input)
-
+  
 (defn work-loop []
   (loop [line (read-line)]
-    (when (not= line nil) 
-      (println (work line)) 
+    (when (not= line nil)
+      (println (work line))
       (recur (read-line)))))
 
 (defn -main []
   (work-loop))
+
+;;(luhny? "56613959932537")
